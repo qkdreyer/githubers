0a6d15980abfc3bd5a4addfc69989cb6d914b7a7
Quentin Dreyer <quentin.dreyer@gmail.com>
2025-01-28 16:50:58 +0100

    chore: use vuejs/core and vitejs/vite as default

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 
 if (location.search.length === 0)
-  location.search = '?repos=vuejs/vue&repos=vuejs/vuex&weeks=52'
+  location.search = '?repos=vuejs/core&repos=vitejs/vite&weeks=52'
 
 createApp(App).mount('#app')
